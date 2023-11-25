2b289c32c2511c88183b3e328b6935801aae1f4e
Lukasz Osuch <lukasz.osuch@iqvia.com>
2023-11-25 14:08:36 +0100

    Login component

--- src/features/Auth/Login.tsx
+++ src/features/Auth/Login.tsx
@@ -1,12 +1,16 @@
-import React, { useState, useContext } from 'react';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Avatar from '@mui/material/Avatar';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
+import { useContext, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-import { Theme } from '@mui/material/styles';
-import { fetchServices } from '../../lib/filipclubApi';
 import NotificationContext from '../../components/NotificationBanner/context';
 import useAuthContext from './authContext';
-import FooterIqviaLogo from '../../assets/footer_ui_logo11.png';
-import { setLoginInfo } from '../../lib/util';
 
 const Login = () => {
   const [token, setToken] = useState('');
@@ -15,34 +19,76 @@ const Login = () => {
   const navigate = useNavigate();
   const [accessToken, setAccessToken] = useAuthContext();
 
-  // const onEnter = async () => {
-  //   setLoading(true);
-  //   const res = await fetchServices(token);
-  //   console.log(res);
-  //   if (res.error) {
-  //     setLoading(false);
-  //     setAlert({
-  //       message: res.message,
-  //       open: true,
-  //       error: true,
-  //     });
-  //     return;
-  //   }
-  //   setAccessToken(token);
-  //   setTimeout(() => {
-  //     setLoading(false);
-  //     navigate('/sources');
-  //     window.localStorage.setItem('token', btoa(token));
-  //     setLoginInfo(res.tenantName, res.tenantId, res.tenantCode);
-  //     setAlert({
-  //       message: 'Successfully LoggedIn',
-  //       open: true,
-  //       error: false,
-  //     });
-  //   }, 200);
-  // };
-
-  return <div>Login Page</div>;
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+  };
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <Box
+        sx={{
+          marginTop: 8,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+          {/* <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Remember me"
+          /> */}
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Sign In
+          </Button>
+          {/* <Grid container>
+            <Grid item xs>
+              <Link href="#" variant="body2">
+                Forgot password?
+              </Link>
+            </Grid>
+            <Grid item>
+              <Link href="#" variant="body2">
+                {"Don't have an account? Sign Up"}
+              </Link>
+            </Grid>
+          </Grid> */}
+        </Box>
+      </Box>
+      {/* <Copyright sx={{ mt: 8, mb: 4 }} /> */}
+    </Container>
+  );
 };
 
 export default Login;
