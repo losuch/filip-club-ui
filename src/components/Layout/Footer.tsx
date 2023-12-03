a9c6c546f98aa748c5301516fce611b7da4b895d
Lukasz Osuch <lukasz.osuch@iqvia.com>
2023-12-03 15:12:45 +0100

    footer alignent left

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -7,7 +7,7 @@ import Typography from '@mui/material/Typography';
 
 function Copyright() {
   return (
-    <Typography variant="body2" color="text.secondary">
+    <Typography variant="body2" color="text.secondary" align="left">
       {'Copyright © '}
       <Link color="inherit" href="https://filip-club.de/">
         filip-club.de
@@ -24,24 +24,7 @@ const defaultTheme = createTheme();
 const Footer = () => {
   return (
     <ThemeProvider theme={defaultTheme}>
-      {/* <Box
-        sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          minHeight: '50vh',
-        }}
-      > */}
       <CssBaseline />
-      {/* <Container component="main" sx={{ mt: 8, mb: 2 }} maxWidth="sm">
-          <Typography variant="h2" component="h1" gutterBottom>
-            Sticky footer
-          </Typography>
-          <Typography variant="h5" component="h2" gutterBottom>
-            {'Pin a footer to the bottom of the viewport.'}
-            {'The footer will move as the main element of the page grows.'}
-          </Typography>
-          <Typography variant="body1">Sticky footer placeholder.</Typography>
-        </Container> */}
       <Box
         component="footer"
         sx={{
@@ -57,7 +40,7 @@ const Footer = () => {
           bottom: 0,
         }}
       >
-        <Container maxWidth="sm">
+        <Container>
           <Copyright />
         </Container>
       </Box>
