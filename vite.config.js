650e3b7a258db226bbc36f90a16434cd48db18b7
Lukasz Osuch <lukasz.osuch@iqvia.com>
2023-12-22 19:36:13 +0100

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,6 @@ import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    host: true,
-    port: 3000
-  },
   base: '/',
   plugins: [svgr({ include: "**/*.svg" }), react()],
   test: {
